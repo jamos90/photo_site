1628b3f02eb53842ff43d1e00e2273f6519ecf43
jamos90 <jamosamos90@gmail.com>
2024-04-06 13:55:00 -0400

    starting image category view

--- src/app/modules/categories/components/category-view/category-view.component.ts
+++ src/app/modules/categories/components/category-view/category-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -7,7 +7,19 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./category-view.component.scss']
 })
 export class CategoryViewComponent {
-  private category: String | null = '';
+  protected category: String | null = '';
+
+  @Input() images: any = [
+    { url: './assets/IMG_6970.jpeg' },
+    { url: './assets/IMG_6976.jpeg' },
+    { url: './assets/397938_10151112237470058_492932457_n.jpg' },
+    { url: './assets/53592_10152223493640058_165711038_o.jpg' },
+    { url: './assets/262702_10150680721425058_7322063_n.jpg' },
+    { url: './assets/339782_10151133670150058_1940421845_o.jpg' },
+    { url: './assets/394721_10151112234690058_1895319742_n.jpg' },
+    { url: './assets/29101657_10160094666705058_3170800344036802560_n.jpg' },
+    { url: './assets/1052235_10152982851815058_1855764683_o.jpg' }
+  ];
 
   constructor(private route: ActivatedRoute) {}
 
