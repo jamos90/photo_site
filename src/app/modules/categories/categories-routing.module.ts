500efc8b7ff23e14247da269dfb1d035df8515fd
jamos90 <jamosamos90@gmail.com>
2024-04-13 14:44:30 -0400

    creates detail view component

--- src/app/modules/categories/categories-routing.module.ts
+++ src/app/modules/categories/categories-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoryViewComponent } from './components/category-view/category-view.component';
+import { DetailViewComponent } from './components/detail-view/detail-view.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CategoryViewComponent,
-    children: [{ path: 'categories/:id', component: CategoryViewComponent }]
+    children: [
+      { path: 'categories/:id', component: CategoryViewComponent },
+      { path: 'categories/:id/:imageId', component: DetailViewComponent }
+    ]
   }
 ];
 
